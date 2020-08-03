9e4db5e8df2efe4b3680c74fb37db35b70fdd610
Adalheidis <marlenaw@protonmail.com>
2020-08-03 20:51:14 +0300

    Update FarmLogCommands.lua

--- FarmLogCommands.lua
+++ FarmLogCommands.lua
@@ -1,19 +1,12 @@
 local farmBar = FarmLogBar;
 
-SLASH_FARMLOG_OPEN1 = "/farmed"
-SLASH_FARMLOG_OPEN2 = "/farmlog"
-SLASH_FARMLOG_TEST1 = "/flnobg"
-SLASH_FARMLOG_TEST2 = "/flnobg"
+SLASH_FARMLOG1 = "/farmed"
+SLASH_FARMLOG2 = "/farmlog"
 
-SlashCmdList["FARMLOG_OPEN"] = function(self, txt)
+SlashCmdList["FARMLOG"] = function(self, txt)
 	if farmBar:IsShown() then
 		farmBar:Hide()
 	else
 		farmBar:Show()
 	end
-end
-
-SlashCmdList["FARMLOG_TEST"] = function(self, txt)
-	farmBar:SetBackdropBorderColor(0,0,0,0)
-	farmBar:SetBackdropColor(0,0,0,0)
 end
